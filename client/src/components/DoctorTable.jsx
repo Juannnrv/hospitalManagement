8455aa06b0cfc5412394eb89817e17f42dea2363
Juannnrv <160557063+Juannnrv@users.noreply.github.com>
2024-10-07 18:43:42 -0500

    feat: :sparkles: DoctorTableRow ready

---
+++ client/src/components/DoctorTable.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import DoctorTableRow from "./DoctorTableRow";
+
+const DoctorTable = ({ doctors }) => {
+  return (
+    <table className="w-full">
+      <thead>
+        <tr className="text-left bg-color-7 px-5 py-3 text-black opacity-45 text-[12px] font-poppins font-semibold">
+          <th className="pb-4">Name</th>
+          <th className="pb-4">ID</th>
+          <th className="pb-4">Email</th>
+          <th className="pb-4">Phone number</th>
+          <th className="pb-4">Date added</th>
+          <th className="pb-4">STATUS</th>
+          <th className="pb-4"></th>
+        </tr>
+      </thead>
+      <tbody>
+        {doctors.map((doctor) => (
+          <DoctorTableRow key={doctor.id} doctor={doctor} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default DoctorTable;
